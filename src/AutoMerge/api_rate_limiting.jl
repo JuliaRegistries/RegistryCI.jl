1fd188bac131a5871282bef5bc9471e557261be4
Dilum Aluthge <dilum@aluthge.com>
2019-10-19 14:27:22 -0400

    Update api_rate_limiting.jl

--- src/AutoMerge/api_rate_limiting.jl
+++ src/AutoMerge/api_rate_limiting.jl
@@ -1,9 +1,9 @@
-function my_retry(f::Function, num_retries::Integer = 20)
+function my_retry(f::Function, num_retries::Integer = 0)
     result = retry(f, delays=ExponentialBackOff(n=num_retries))()
     return result
 end
 
-function my_retry_suppress_exceptions(f::Function, num_retries::Integer = 20)
+function my_retry_suppress_exceptions(f::Function, num_retries::Integer = 0)
     result = try
         my_retry(f, num_retries)
     catch ex
