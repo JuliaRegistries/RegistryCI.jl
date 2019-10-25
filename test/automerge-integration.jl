28706c746e3ae5e098c17b9f5134057b18a2f99c
Dilum Aluthge <dilum@aluthge.com>
2019-10-25 19:34:01 -0400

    Authenticate as test user during integration tests (#57)

--- test/automerge-integration.jl
+++ test/automerge-integration.jl
@@ -1,1 +1,10 @@
+include("automerge-integration-utils.jl")
 
+AUTOMERGE_INTEGRATION_TEST_REPO = ENV["AUTOMERGE_INTEGRATION_TEST_REPO"]::String
+TEST_USER_GITHUB_TOKEN = ENV["BCBI_TEST_USER_GITHUB_TOKEN"]::String
+auth = GitHub.authenticate(TEST_USER_GITHUB_TOKEN)
+whoami = RegistryCI.AutoMerge.username(auth)
+repo_url_without_auth = "https://github.com/$(AUTOMERGE_INTEGRATION_TEST_REPO)"
+repo_url_with_auth = "https://$(whoami):$(TEST_USER_GITHUB_TOKEN)@github.com/$(AUTOMERGE_INTEGRATION_TEST_REPO)"
+repo = GitHub.repo(AUTOMERGE_INTEGRATION_TEST_REPO; auth = auth)
+@info("Authenticated to GitHub as \"$(whoami)\"")
