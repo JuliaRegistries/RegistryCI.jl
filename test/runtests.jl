9416757eb68edd87ad76bad8b763fc5978eae7ba
Dilum Aluthge <dilum@aluthge.com>
2019-11-09 01:21:53 -0500

    Always run the unit tests (#100)

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,20 +9,21 @@ using TimeZones
 const AutoMerge = RegistryCI.AutoMerge
 
 @testset "RegistryCI.jl" begin
+    @testset "RegistryCI.jl unit tests" begin
+        @info("Running the RegistryCI.jl unit tests")
+        include("registryci.jl")
+    end
+
+    @testset "AutoMerge.jl unit tests" begin
+        @info("Running the AutoMerge.jl unit tests")
+        include("automerge-unit.jl")
+    end
+
     AUTOMERGE_RUN_INTEGRATION_TESTS = get(ENV, "AUTOMERGE_RUN_INTEGRATION_TESTS", "")::String
     if AUTOMERGE_RUN_INTEGRATION_TESTS == "true"
         @testset "AutoMerge.jl integration tests" begin
             @info("Running the AutoMerge.jl integration tests")
             include("automerge-integration.jl")
-        end
-    else
-        @testset "RegistryCI.jl unit tests" begin
-            @info("Running the RegistryCI.jl unit tests")
-            include("registryci.jl")
-        end
-        @testset "AutoMerge.jl unit tests" begin
-            @info("Running the AutoMerge.jl unit tests")
-            include("automerge-unit.jl")
         end
     end
 end
