c6611d80ec7096d3d874635bc0c0e0e471892459
Dilum Aluthge <dilum@aluthge.com>
2019-11-13 17:42:26 -0500

    Exit with status code 1 if guidelines were not met

--- src/AutoMerge/new-version.jl
+++ src/AutoMerge/new-version.jl
@@ -23,7 +23,7 @@ function pull_request_build(::NewVersion,
     if is_open(pr)
         if pr_author_login in authorized_authors
             my_retry(() -> delete_all_of_my_reviews!(registry, pr; auth = auth, whoami = whoami))
-            my_retry(() -> GitHub.create_status(registry, current_pr_head_commit_sha; auth=auth, params=Dict("state" => "pending", "context" => "automerge/new-version")))
+            my_retry(() -> GitHub.create_status(registry, current_pr_head_commit_sha; auth=auth, params=Dict("state" => "pending", "context" => "automerge/decision")))
             g0, m0 = pr_only_changes_allowed_files(NewVersion(), registry, pr, pkg; auth = auth)
             newv_g1, newv_m1, release_type = meets_sequential_version_number(pkg,
                                                                              version;
@@ -57,7 +57,7 @@ function pull_request_build(::NewVersion,
                                                              version)
                     my_retry(() -> delete_all_of_my_reviews!(registry, pr; auth = auth, whoami = whoami))
                     my_retry(() -> approve!(registry, pr, current_pr_head_commit_sha; auth = auth, body = newv_commenttextpass, whoami = whoami))
-                    my_retry(() -> GitHub.create_status(registry, current_pr_head_commit_sha; auth=auth, params=Dict("state" => "success", "context" => "automerge/new-version", "description" => "$(current_pr_head_commit_sha)")))
+                    my_retry(() -> GitHub.create_status(registry, current_pr_head_commit_sha; auth=auth, params=Dict("state" => "success", "context" => "automerge/decision", "description" => "$(current_pr_head_commit_sha)")))
                     return nothing
                 else
                     newv_commenttext4and5 = comment_text_fail(NewVersion(),
@@ -65,7 +65,8 @@ function pull_request_build(::NewVersion,
                                                               suggest_onepointzero,
                                                               version)
                     my_retry(() -> post_comment!(registry, pr, newv_commenttext4and5; auth = auth))
-                    my_retry(() -> GitHub.create_status(registry, current_pr_head_commit_sha; auth=auth, params=Dict("state" => "failure", "context" => "automerge/new-version")))
+                    my_retry(() -> GitHub.create_status(registry, current_pr_head_commit_sha; auth=auth, params=Dict("state" => "failure", "context" => "automerge/decision")))
+                    error("The automerge guidelines were not met.")
                     return nothing
                 end
             else
@@ -76,7 +77,8 @@ function pull_request_build(::NewVersion,
                                                               suggest_onepointzero,
                                                               version)
                 my_retry(() -> post_comment!(registry, pr, newv_commenttext1through3; auth = auth))
-                my_retry(() -> GitHub.create_status(registry, current_pr_head_commit_sha; auth=auth, params=Dict("state" => "failure", "context" => "automerge/new-version")))
+                my_retry(() -> GitHub.create_status(registry, current_pr_head_commit_sha; auth=auth, params=Dict("state" => "failure", "context" => "automerge/decision")))
+                error("The automerge guidelines were not met.")
                 return nothing
             end
         else
