45bfd582c44fd055a2a6687b65dab6a08d826b7b
Dilum Aluthge <dilum@aluthge.com>
2019-12-27 19:05:16 -0500

    Automerge JLL packages (#153)

--- src/AutoMerge/new-package.jl
+++ src/AutoMerge/new-package.jl
@@ -20,15 +20,35 @@ function pull_request_build(::NewPackage,
     #     - `E/Example/Deps.toml`
     #     - `E/Example/Package.toml`
     #     - `E/Example/Versions.toml`
-    # 2. Normal capitalization - name should match r"^[A-Z]\w*[a-z][0-9]?$" - i.e. starts with a capital letter, ASCII alphanumerics only, ends in lowercase
-    # 3. Not too short - at least five letters - you can register names shorter than this, but doing so requires someone to approve
-    # 4. Standard initial version number - one of 0.0.1, 0.1.0, 1.0.0
-    # 5. Repo URL ends with /$name.jl.git where name is the package name
-    # 6. Compat for all dependencies - all [deps] should also have [compat] entries (and Julia itself) - [compat] entries should have upper bounds
-    # 7. Version can be installed - given the proposed changes to the registry, can we resolve and install the new version of the package?
-    # 8. Version can be loaded - once it's been installed (and built?), can we load the code?
+    # 2. TODO: implement this check.
+    # 3. Normal capitalization
+    #     - name should match r"^[A-Z]\w*[a-z][0-9]?$"
+    #     - i.e. starts with a capital letter, ASCII alphanumerics only, ends in lowercase
+    # 4. Not too short
+    #     - at least five letters
+    #     - you can register names shorter than this, but doing so requires someone to approve
+    # 5. Standard initial version number - one of 0.0.1, 0.1.0, 1.0.0
+    #     - does not apply to JLL packages
+    # 6. Repo URL ends with /$name.jl.git where name is the package name
+    # 7. Compat for all dependencies
+    #     - there should be a [compat] entry for Julia
+    #     - all [deps] should also have [compat] entries
+    #     - all [compat] entries should have upper bounds
+    #     - dependencies that are standard libraries do not need [compat] entries
+    #     - dependencies that are JLL packages do not need [compat] entries
+    # 8. (only applies to JLL packages) The only dependencies of the package are:
+    #     - Pkg
+    #     - Libdl
+    #     - other JLL packages
+    # 9. Version can be installed
+    #     - given the proposed changes to the registry, can we resolve and install the new version of the package?
+    #     - i.e. can we run `Pkg.add("Foo")`
+    # 10. Version can be loaded
+    #     - once it's been installed (and built?), can we load the code?
+    #     - i.e. can we run `import Foo`
     pkg, version = parse_pull_request_title(NewPackage(), pr)
-    @info("This is a new package pull request", pkg, version)
+    this_is_jll_package = is_jll_name(pkg)
+    @info("This is a new package pull request", pkg, version, this_is_jll_package)
     pr_author_login = author_login(pr)
     if is_open(pr)
         if pr_author_login in authorized_authors
@@ -49,35 +69,52 @@ function pull_request_build(::NewPackage,
                                                    pr,
                                                    pkg;
                                                    auth = auth)
-            g2, m2 = meets_normal_capitalization(pkg)
-            g3, m3 = meets_name_length(pkg)
-            g4, m4 = meets_standard_initial_version_number(version)
-            g5, m5 = meets_repo_url_requirement(pkg;
+            g2 = true
+            m2 = ""
+            g3, m3 = meets_normal_capitalization(pkg)
+            g4, m4 = meets_name_length(pkg)
+            g5, m5 = meets_standard_initial_version_number(version)
+            g6, m6 = meets_repo_url_requirement(pkg;
                                                 registry_head = registry_head)
-            g6, m6 = meets_compat_for_all_deps(registry_head,
+            g7, m7 = meets_compat_for_all_deps(registry_head,
                                                pkg,
                                                version)
-
+            g8_if_jll, m8_if_jll = meets_allowed_jll_nonrecursive_dependencies(registry_head,
+                                                                               pkg,
+                                                                               version)
+            if this_is_jll_package
+                g8 = g8_if_jll
+                m8 = m8_if_jll
+            else
+                g8 = true
+                m8 = ""
+            end
             @info("Only modifies the files that it's allowed to modify",
                   meets_this_guideline = g1,
                   message = m1)
-            @info("Normal capitalization",
+            @info("TODO: implement this check",
                   meets_this_guideline = g2,
                   message = m2)
-            @info("Name not too short",
+            @info("Normal capitalization",
                   meets_this_guideline = g3,
                   message = m3)
-            @info("Standard initial version number ",
+            @info("Name not too short",
                   meets_this_guideline = g4,
                   message = m4)
-            @info("Repo URL ends with /name.jl.git",
+            @info("Standard initial version number ",
                   meets_this_guideline = g5,
                   message = m5)
-            @info("Compat (with upper bound) for all dependencies",
+            @info("Repo URL ends with /name.jl.git",
                   meets_this_guideline = g6,
                   message = m6)
-            g1through6 = [g1, g2, g3, g4, g5, g6]
-            if !all(g1through6)
+            @info("Compat (with upper bound) for all dependencies",
+                  meets_this_guideline = g7,
+                  message = m7)
+            @info("If this is a JLL package, only deps are Pkg, Libdl, and other JLL packages",
+                  meets_this_guideline = g8,
+                  message = m8)
+            g1through8 = [g1, g2, g3, g4, g5, g6, g7, g8]
+            if !all(g1through8)
                 description = "New package. Failed."
                 params = Dict("state" => "failure",
                               "context" => "automerge/decision",
@@ -87,21 +124,21 @@ function pull_request_build(::NewPackage,
                                                     auth = auth,
                                                     params = params))
             end
-            g7, m7 = meets_version_can_be_pkg_added(registry_head,
+            g9, m9 = meets_version_can_be_pkg_added(registry_head,
                                                     pkg,
                                                     version)
             @info("Version can be `Pkg.add`ed",
-                  meets_this_guideline = g7,
-                  message = m7)
-            g8, m8 = meets_version_can_be_imported(registry_head,
+                  meets_this_guideline = g9,
+                  message = m9)
+            g10, m10 = meets_version_can_be_imported(registry_head,
                                                    pkg,
                                                    version)
             @info("Version can be `import`ed",
-                  meets_this_guideline = g8,
-                  message = m8)
-            g1through8 = [g1, g2, g3, g4, g5, g6, g7, g8]
-            allmessages1through8 = [m1, m2, m3, m4, m5, m7, m8]
-            if all(g1through8) # success
+                  meets_this_guideline = g9,
+                  message = m9)
+            g1through10 = [g1, g2, g3, g4, g5, g6, g7, g8, g9, g10]
+            allmessages1through10 = [m1, m2, m3, m4, m5, m7, m8, m9, m10]
+            if all(g1through10) # success
                 description = "New package. Approved. sha=\"$(current_pr_head_commit_sha)\""
                 params = Dict("state" => "success",
                               "context" => "automerge/decision",
@@ -133,9 +170,9 @@ function pull_request_build(::NewPackage,
                                                     current_pr_head_commit_sha;
                                                     auth = auth,
                                                     params = params))
-                failingmessages1through8 = allmessages1through8[.!g1through8]
+                failingmessages1through10 = allmessages1through10[.!g1through10]
                 this_pr_comment_fail = comment_text_fail(NewPackage(),
-                                                         failingmessages1through8,
+                                                         failingmessages1through10,
                                                          suggest_onepointzero,
                                                          version)
                 my_retry(() -> post_comment!(registry,
