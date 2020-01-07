4c44c3f35f191cfa4fd10a85c9fd0ae96e471bd0
Dilum Aluthge <dilum@aluthge.com>
2020-01-07 02:39:19 -0500

    Fix tests

--- test/registryci_registry_testing.jl
+++ test/registryci_registry_testing.jl
@@ -14,7 +14,7 @@ const path = joinpath(DEPOT_PATH[1], "registries", "General")
 RegistryCI.test(path)
 
 # Test the BioJuliaRegistry
-with_temp_depot() do
+RegistryCI.with_temp_depot() do
     Pkg.Registry.add(Pkg.RegistrySpec(url = "https://github.com/BioJulia/BioJuliaRegistry.git"))
     # Test this will validate the BioJuliaRegistry, when providing General as an
     # optional dependency. BJW.
