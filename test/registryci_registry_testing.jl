21b08f5272fbbc578ba560e1876e0f27b6d6bb5e
Dilum Aluthge <dilum@aluthge.com>
2020-01-07 02:30:51 -0500

    Add `BioJuliaRegistry` inside a temp depot, and fix tests

--- test/registryci_registry_testing.jl
+++ test/registryci_registry_testing.jl
@@ -14,8 +14,10 @@ const path = joinpath(DEPOT_PATH[1], "registries", "General")
 RegistryCI.test(path)
 
 # Test the BioJuliaRegistry
-Pkg.Registry.add(Pkg.RegistrySpec(url = "https://github.com/BioJulia/BioJuliaRegistry.git"))
-# Test this will validate the BioJuliaRegistry, when providing General as an
-# optional dependency. BJW.
-const biopath = joinpath(DEPOT_PATH[1], "registries", "BioJuliaRegistry")
-RegistryCI.test(biopath, ["https://github.com/JuliaRegistries/General.git"])
+with_temp_depot() do
+    Pkg.Registry.add(Pkg.RegistrySpec(url = "https://github.com/BioJulia/BioJuliaRegistry.git"))
+    # Test this will validate the BioJuliaRegistry, when providing General as an
+    # optional dependency. BJW.
+    biopath = joinpath(DEPOT_PATH[1], "registries", "BioJuliaRegistry")
+    RegistryCI.test(biopath, registry_deps = ["https://github.com/JuliaRegistries/General.git"])
+end
