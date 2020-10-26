4261af0280361638f4b83f32690b17a5bfeef4b1
Chris de Graaf <me@cdg.dev>
2020-10-26 16:18:16 -0500

    Add daily cron functionality

--- src/TagBot/TagBot.jl
+++ src/TagBot/TagBot.jl
@@ -1,15 +1,15 @@
 module TagBot
 
+using Dates: Day, UTC, now
+
 using GitHub: GitHub
 using JSON: JSON
 
 const GH = GitHub
 
-const ISSUE_TITLE = "TagBot trigger issue"
-const ISSUE_BODY = "This issue is used to trigger TagBot; feel free to unsubscribe from it."
-
 const AUTH = Ref{GH.OAuth2}()
-const USER = Ref{String}()
+const ISSUE_TITLE = "TagBot trigger issue"
+const ISSUE_BODY = "This issue is used to trigger TagBot; feel free to unsubscribe."
 
 function main()
     AUTH[] = GH.authenticate(ENV["GITHUB_TOKEN"])
@@ -25,9 +25,16 @@ is_merged_pull_request(event) = event["pull_request"]["merged"]
 
 is_cron(event) = get(ENV, "GITHUB_EVENT_NAME", "") == "schedule"
 
-function repo_of_pull_request_body(body)
+function repo_and_version_of_pull_request_body(body)
+    if occursin("JLL package", body)
+        @info "Skipping JLL package registration"
+        return nothing, nothing
+    end
     m = match(r"Repository: .*github\.com[:/](.*)", body)
-    return m === nothing ? nothing : m[1]
+    repo = m === nothing ? nothing : m[1]
+    version = match(r"Version: (.*)", body)
+    version = m === nothing ? nothing : m[1]
+    return repo, version
 end
 
 function clone_repo(repo)
@@ -75,9 +82,12 @@ function notify(repo, issue, body)
 end
 
 function handle_merged_pull_request(event)
-    repo = repo_of_pull_request_body(event["pull_request"]["body"])
+    number = event["pull_request"]["number"]
+    @info "Processing pull request $number"
+    repo, version = repo_and_version_of_pull_request_body(event["pull_request"]["body"])
+    @info "Release $version of $repo"
     if repo === nothing
-        @info "Failed to parse GitHub repository from merge request"
+        @info "Failed to parse GitHub repository from pull request"
         return
     end
     @info "Processing merged PR for $repo"
@@ -90,7 +100,60 @@ function handle_merged_pull_request(event)
     notify(repo, issue, body)
 end
 
+function collect_pulls(repo)
+    acc = GH.PullRequest[]
+    params = (; state="closed", sort="updated", direction="desc", per_page=100)
+    kwargs = Dict(:auth => AUTH[], :params => params, :page_limit => 1)
+    done = false
+    while !done
+        pulls, pages = GH.pull_requests(repo; kwargs...)
+        for pull in pulls
+            pull.merged_at === nothing && continue
+            if now(UTC) - pull.merged_at < Day(1)
+                push!(acc, pull)
+            else
+                done = true
+            end
+        end
+        if haskey(pages, "next")
+            delete!(kwargs, :params)
+            kwargs[:start_page] = pages["next"]
+        else
+            done = true
+        end
+    end
+    return acc
+end
+
+function tag_exists(repo, version)
+    return try
+        GH.tag(repo, version; auth=AUTH[])
+        true
+    catch e
+        if !occursin("404", string(e))
+            @warn "Unknown error when checking for existing tag" ex=(e, catch_backtrace())
+        end
+        false
+    end
+end
+
 function handle_cron(event)
+    pulls = collect_pulls(event["repository"]["full_name"])
+    for pull in pulls
+        repo, version = repo_and_version_of_pull_request_body(pull.body)
+        if repo === nothing
+            @info "Failed to parse GitHub repository from pull reqest"
+            continue
+        end
+        @info "Processing version $version of $repo"
+        if tag_exists(repo, version)
+            @info "Tag $version already exists for $repo"
+            continue
+        end
+        issue = get_repo_notification_issue(repo)
+        body = notification_body(event)
+        notify(repo, issue, body)
+    end
 end
 
 end
