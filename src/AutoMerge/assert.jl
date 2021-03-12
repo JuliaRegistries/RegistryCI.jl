73a17a967e2de7290eaa3cfbd7314d5bd641110f
Dilum Aluthge <dilum@aluthge.com>
2021-03-12 02:12:10 -0500

    Remove all uses of the `Base.@assert` macro

--- src/AutoMerge/assert.jl
+++ src/AutoMerge/assert.jl
@@ -3,10 +3,10 @@ struct AlwaysAssertionError <: Exception
 end
 AlwaysAssertionError() = AlwaysAssertionError("")
 
-# The documentation for the `@assert` macro says: "Warning: An assert might be
+# The documentation for the `Base.@assert` macro says: "Warning: An assert might be
 # disabled at various optimization levels."
 # Therefore, we have the `always_assert` function. `always_assert` is like
-# `@assert`, except that `always_assert` will always run and will never be
+# `Base.@assert`, except that `always_assert` will always run and will never be
 # disabled.
 function always_assert(cond::Bool)
     cond || throw(AlwaysAssertionError())
