a42805ee9ade9cfe1698edd629460f624cf2b2aa
Dilum Aluthge <dilum@aluthge.com>
2021-06-06 19:58:35 -0400

    Fix the test suite on Julia nightly

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,12 @@ using TimeZones
 
 const AutoMerge = RegistryCI.AutoMerge
 
+# Starting with Julia 1.7, when you use the Pkg server registry, the registry tarball does
+# not get unpacked, and thus the registry files are not available. Of course, RegistryCI
+# requires that the registry files are available. So for the RegistryCI test suite, we will
+# disable the Pkg server.
+ENV["JULIA_PKG_SERVER"] = ""
+
 @testset "RegistryCI.jl" begin
     @testset "RegistryCI.jl unit tests" begin
         @info("Running the RegistryCI.jl unit tests")
