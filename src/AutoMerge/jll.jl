76d1f800861dc5cdec8e0f5c383b12625f08fa07
Mosè Giordano <giordano@users.noreply.github.com>
2022-03-23 12:51:30 +0000

    Allow TOML as dependency of JLL packages

--- src/AutoMerge/jll.jl
+++ src/AutoMerge/jll.jl
@@ -36,9 +36,10 @@ function meets_allowed_jll_nonrecursive_dependencies(
     # 3. Artifacts
     # 4. JLLWrappers
     # 5. LazyArtifacts
-    # 6. other JLL packages
+    # 6. TOML
+    # 7. other JLL packages
     all_dependencies = _get_all_dependencies_nonrecursive(working_directory, pkg, version)
-    allowed_dependencies = ("Pkg", "Libdl", "Artifacts", "JLLWrappers", "LazyArtifacts")
+    allowed_dependencies = ("Pkg", "Libdl", "Artifacts", "JLLWrappers", "LazyArtifacts", "TOML")
     for dep in all_dependencies
         if dep ∉ allowed_dependencies && !is_jll_name(dep)
             return false,
