27a210702941777a3b2295d387dd05653ebc32bd
Dilum Aluthge <dilum@aluthge.com>
2023-02-21 08:43:46 -0500

    Remove some asserts

--- src/AutoMerge/toml.jl
+++ src/AutoMerge/toml.jl
@@ -1,16 +1,1 @@
-function assert_allowed_to_not_exist(full_path::AbstractString)
-    file = basename(full_path)
-    allowed_filenames = (
-        "Compat.toml",
-        "WeakCompat.toml",
-        "Deps.toml",
-        "WeakDeps.toml",
-    )
-    if !(file in allowed_filenames)
-        msg = "Filename is not in the allowed list: $(file)"
-        throw(ErrorException(msg))
-    end
-    return nothing
-end
-
 maybe_parse_toml(f::AbstractString) = ispath(f) ? Pkg.TOML.parsefile(f) : Dict{String, Any}()
