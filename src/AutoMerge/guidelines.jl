3f6f103c554a9fd7b7d72d67df5b92bda9df1bce
Shayan <sh0davoodi@gmail.com>
2023-11-22 23:44:32 +0330

    replace 'recommended' by 'required'.

--- src/AutoMerge/guidelines.jl
+++ src/AutoMerge/guidelines.jl
@@ -425,7 +425,7 @@ end
 
 function _invalid_sequential_version(reason::AbstractString)
     return false, "Does not meet sequential version number guideline: $reason\n \
-    A double check is recommended to verify this was done on purpose.", :invalid
+    A double check is required to verify this was done on purpose.", :invalid
 end
 
 function _valid_change(old_version::VersionNumber, new_version::VersionNumber)
