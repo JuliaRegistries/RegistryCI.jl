0dc9919803a23e91c1c350ee0edd64ee2057d957
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-01-31 00:54:27 +0100

    fix typo in test

--- test/automerge-unit.jl
+++ test/automerge-unit.jl
@@ -108,7 +108,7 @@ end
     end
     @testset "Package name match check" begin
         @test AutoMerge.meets_name_match_check("Flux", ["Abc", "Def"])[1]
-        @test !AutoMerge.meets_distance_check("Websocket", ["websocket"])[1]
+        @test !AutoMerge.meets_name_match_check("Websocket", ["websocket"])[1]
     end
     @testset "Package name distance" begin
         @test AutoMerge.meets_distance_check("Flux", ["Abc", "Def"])[1]
