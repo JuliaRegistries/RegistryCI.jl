3095c882be5361e03f877e901d0efff4cfdadb42
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-06-12 23:17:09 +0200

    fix missed bit from revert

--- test/automerge-integration.jl
+++ test/automerge-integration.jl
@@ -177,7 +177,7 @@ hello_world_commit2 = "57b0aec49622faa962c6752d4bc39a62b91fe37c"
             requires_commit,
         ), # FAIL: new package name is not a Julia identifier
     ])
-        @info "Performing integration tests with settings" test_number master_dir feature_dir public_dir title point_to_slack check_license pass pass_consistency commit
+        @info "Performing integration tests with settings" test_number master_dir feature_dir public_dir title point_to_slack check_license pass commit
         with_master_branch(
             templates(master_dir), "master"; GIT=GIT, repo_url=repo_url_with_auth
         ) do master
@@ -235,7 +235,7 @@ hello_world_commit2 = "57b0aec49622faa962c6752d4bc39a62b91fe37c"
                                 check_license=check_license,
                                 public_registries=public_registries,
                             )
-                        @info "Running integration test for " test_number master_dir feature_dir public_dir title point_to_slack check_license pass pass_consistency commit
+                        @info "Running integration test for " test_number master_dir feature_dir public_dir title point_to_slack check_license pass commit
                         if pass
                             run_thunk()
                         else
