1fce0c601e16cae65a2a093d029be109309f9a37
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2025-09-25 17:03:55 +0200

    Remove caching of parsed toml files.

--- AutoMerge.jl/src/toml.jl
+++ AutoMerge.jl/src/toml.jl
@@ -1,16 +1,6 @@
-const _toml_cache = Dict{String, Any}()
-
-# Cache parsed toml files for efficiency.
-# Important: Do not mutate the returned data, as it will corrupt the cache.
 function parse_registry_toml(registry_dir, path_components...; allow_missing = false)
     path = joinpath(registry_dir, path_components...)
-    get!(_toml_cache, path) do
-        if isfile(path)
-            TOML.parsefile(path)
-        elseif allow_missing
-            Dict{String, Any}()
-        else
-            error("Registry file $path does not exist in $(registry_dir).")
-        end
-    end
+    isfile(path) && return TOML.parsefile(path)
+    allow_missing && return Dict{String, Any}()
+    error("Registry file $path does not exist in $(registry_dir).")
 end
