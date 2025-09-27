1f666fc4585ffa602ebb309dfd1ff3bb9bbda7c8
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2025-09-27 02:19:50 +0200

    AutoMerge: Refactor parsing of registry toml files. (#614)
    * Refactor and cache parsing of registry toml files.
    
    * Remove caching of parsed toml files.
    
    ---------
    
    Co-authored-by: Eric Hanson <5846501+ericphanson@users.noreply.github.com>

--- AutoMerge.jl/src/toml.jl
+++ AutoMerge.jl/src/toml.jl
@@ -1,1 +1,6 @@
-maybe_parse_toml(f::AbstractString) = ispath(f) ? Pkg.TOML.parsefile(f) : Dict{String, Any}()
+function parse_registry_toml(registry_dir, path_components...; allow_missing = false)
+    path = joinpath(registry_dir, path_components...)
+    isfile(path) && return TOML.parsefile(path)
+    allow_missing && return Dict{String, Any}()
+    error("Registry file $path does not exist in $(registry_dir).")
+end
