91e410965d307402048a0154049fdea8d37abe06
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-10-15 14:55:15 +0200

    account better for read-only mode

--- AutoMerge/src/automerge_comment.jl
+++ AutoMerge/src/automerge_comment.jl
@@ -1,11 +1,15 @@
 function update_automerge_comment!(data::GitHubAutoMergeData, body::AbstractString)::Nothing
-    @info """Creating or updating automerge comment with contents:
-
-    $body"""
     if data.read_only
-        @info "`read_only` mode; skipping updating automerge comment."
+        @info """
+        `read_only` mode; skipping updating automerge comment with contents:
+
+        $body"""
         return nothing
     end
+    @info """
+    Creating or updating automerge comment with contents:
+
+    $body"""
     api = data.api
     repo = data.registry
     pr = data.pr
