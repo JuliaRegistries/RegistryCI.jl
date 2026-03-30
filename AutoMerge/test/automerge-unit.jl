8839eb0855286d28005a7ea8567481c3b32bde81
Dilum Aluthge <dilum@aluthge.com>
2026-03-30 02:44:09 -0400

    Update automerge-unit.jl

--- AutoMerge/test/automerge-unit.jl
+++ AutoMerge/test/automerge-unit.jl
@@ -731,11 +731,11 @@ end
         # Maybe this should fail as the label isn't applied by JuliaRegistrator, so the version isn't breaking?
         @test AutoMerge.meets_breaking_explanation_check([], body_good)[1]
 
-        @test AutoMerge.meets_breaking_explanation_check(v"1.0.0", [breaking_label], body_bad)[1]
-        @test AutoMerge.meets_breaking_explanation_check(v"1.2.3", [breaking_label], body_bad_no_notes)[1]
-        @test !AutoMerge.meets_breaking_explanation_check(v"0.3.0", [breaking_label], body_bad)[1]
-        @test !AutoMerge.meets_breaking_explanation_check(v"2.0.0", [breaking_label], body_bad_no_notes)[1]
-        @test AutoMerge.meets_breaking_explanation_check(v"2.0.0", [breaking_label], body_good)[1]
+        @test AutoMerge.meets_breaking_explanation_check([breaking_label], body_bad, v"1.0.0")[1]
+        @test AutoMerge.meets_breaking_explanation_check([breaking_label], body_bad_no_notes, v"1.2.3")[1]
+        @test !AutoMerge.meets_breaking_explanation_check([breaking_label], body_bad, v"0.3.0")[1]
+        @test !AutoMerge.meets_breaking_explanation_check([breaking_label], body_bad_no_notes, v"2.0.0")[1]
+        @test AutoMerge.meets_breaking_explanation_check([breaking_label], body_good, v"2.0.0")[1]
     end
 end
 
